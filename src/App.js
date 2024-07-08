ee092fe98144dc143ae3d709dfacdb61aaa08599
Samir Singh <168593395+samir54883@users.noreply.github.com>
2024-07-09 01:13:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import ConversationalAIPage from './pages/ConversationalAIPage';
+import FeaturesPage from './pages/FeaturesPage';
+import TrainingPage from './pages/TrainingPage';
+import ResourcesPage from './pages/ResourcesPage';
+import CommunityPage from './pages/CommunityPage';
+import ContactUsPage from './pages/ContactUsPage';
+import LoginSignupPage from './pages/LoginSignupPage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Header />
+            <Switch>
+                <Route path="/" exact component={HomePage} />
+                <Route path="/conversational-ai" component={ConversationalAIPage} />
+                <Route path="/features" component={FeaturesPage} />
+                <Route path="/training" component={TrainingPage} />
+                <Route path="/resources" component={ResourcesPage} />
+                <Route path="/community" component={CommunityPage} />
+                <Route path="/contact-us" component={ContactUsPage} />
+                <Route path="/login-signup" component={LoginSignupPage} />
+            </Switch>
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
